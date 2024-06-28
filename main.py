994c6369fab18c2f14d581c3aa0cd5a0318d9f93
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-06-28 06:16:27 +0100

    Add The-InkWell project

---
+++ main.py
@@ -0,0 +1,163 @@
+from flask import Flask, render_template, request, redirect, session, flash
+from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+from flask_ckeditor import CKEditor
+import yaml
+from yaml import Loader  # Import Loader class
+
+# Initialize the Flask application
+app = Flask(__name__)
+Bootstrap(app)  # Integrate Bootstrap for styling
+ckeditor = CKEditor(app)  # Integrate CKEditor for rich text editing
+
+# Load database configuration from a YAML file
+db = yaml.load(open('mysql-conn.yaml'), Loader=Loader)
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)  # Initialize MySQL with the app configuration
+
+app.config['SECRET_KEY'] = 'secret'  # Set a secret key for session management
+
+@app.route('/')
+def index():
+    """Route for the homepage that displays all blogs."""
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM blog")
+    if resultValue > 0:
+        blogs = cur.fetchall()
+        cur.close()
+        return render_template('index.html', blogs=blogs)
+    cur.close()
+    return render_template('index.html', blogs=None)
+
+@app.route('/about/')
+def about():
+    """Route for the 'About' page."""
+    return render_template('about.html')
+
+@app.route('/blogs/<int:id>/')
+def blogs(id):
+    """Route to display a specific blog post by its ID."""
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM blog WHERE blog_id = {}".format(id))
+    if resultValue > 0:
+        blog = cur.fetchone()
+        return render_template('blog.html', blog=blog)
+    return 'Blog not found'
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    """Route for user registration."""
+    if request.method == 'POST':
+        userDetails = request.form
+        if userDetails['password'] != userDetails['confirm_password']:
+            flash('Passwords do not match! Try again.', 'danger')
+            return render_template('register.html')
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO user(first_name, last_name, username, email, password) "\
+        "VALUES(%s,%s,%s,%s,%s)",(userDetails['first_name'], userDetails['last_name'], \
+        userDetails['username'], userDetails['email'], userDetails['password']))
+        mysql.connection.commit()
+        cur.close()
+        flash('Registration successful! Please login.', 'success')
+        return redirect('/login')
+    return render_template('register.html')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    """Route for user login."""
+    if request.method == 'POST':
+        userDetails = request.form
+        username = userDetails['username']
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute("SELECT * FROM user WHERE username = %s", ([username]))
+        if resultValue > 0:
+            user = cur.fetchone()
+            if userDetails['password'] == user['password']:
+                session['login'] = True
+                session['firstName'] = user['first_name']
+                session['lastName'] = user['last_name']
+                flash('Welcome ' + session['firstName'] +'! You have been successfully logged in', 'success')
+            else:
+                cur.close()
+                flash('Password does not match', 'danger')
+                return render_template('login.html')
+        else:
+            cur.close()
+            flash('User not found', 'danger')
+            return render_template('login.html')
+        cur.close()
+        return redirect('/')
+    return render_template('login.html')
+
+@app.route('/write-blog/',methods=['GET', 'POST'])
+def write_blog():
+    """Route to write a new blog post."""
+    if request.method == 'POST':
+        blogpost = request.form
+        title = blogpost['title']
+        body = blogpost['body']
+        author = session['firstName'] + ' ' + session['lastName']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO blog(title, body, author) VALUES(%s, %s, %s)", (title, body, author))
+        mysql.connection.commit()
+        cur.close()
+        flash("Successfully posted new blog", 'success')
+        return redirect('/')
+    return render_template('write_blog.html')
+
+@app.route('/my-blogs/')
+def view_blogs():
+    """Route to view all blogs written by the logged-in user."""
+    author = session['firstName'] + ' ' + session['lastName']
+    cur = mysql.connection.cursor()
+    result_value = cur.execute("SELECT * FROM blog WHERE author = %s",[author])
+    if result_value > 0:
+        my_blogs = cur.fetchall()
+        return render_template('my_blogs.html',my_blogs=my_blogs)
+    else:
+        return render_template('my_blogs.html',my_blogs=None)
+
+@app.route('/edit-blog/<int:id>/', methods=['GET', 'POST'])
+def edit_blog(id):
+    """Route to edit an existing blog post."""
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        title = request.form['title']
+        body = request.form['body']
+        cur.execute("UPDATE blog SET title = %s, body = %s where blog_id = %s",(title, body, id))
+        mysql.connection.commit()
+        cur.close()
+        flash('Blog updated successfully', 'success')
+        return redirect('/blogs/{}'.format(id))
+    cur = mysql.connection.cursor()
+    result_value = cur.execute("SELECT * FROM blog WHERE blog_id = {}".format(id))
+    if result_value > 0:
+        blog = cur.fetchone()
+        blog_form = {}
+        blog_form['title'] = blog['title']
+        blog_form['body'] = blog['body']
+        return render_template('edit_blog.html', blog_form=blog_form)
+
+@app.route('/delete-blog/<int:id>/')
+def delete_blog(id):
+    """Route to delete a blog post."""
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM blog WHERE blog_id = {}".format(id))
+    mysql.connection.commit()
+    flash("Your blog has been deleted", 'success')
+    return redirect('/my-blogs')
+
+@app.route('/logout/')
+def logout():
+    """Route to log out the user."""
+    session.clear()
+    flash("You have been logged out", 'info')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
+
